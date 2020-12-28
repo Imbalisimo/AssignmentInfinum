43c41adab976ee2afaca69ba820bedc1ec881ec1
Marin Pepeljnjak <marin9619@hotmail.com>
2020-12-28 21:32:32 +0100

    implement database and controller calling

---
+++ AssignmentInfinum/ContactsDatabase.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssignmentInfinum
+{
+    public class ContactsDatabase
+    {
+        ContactsEntities1 contactsEntities;
+        System.Data.Common.DbConnection connection;
+
+        public static ContactsDatabase Instance { get; } = new ContactsDatabase();
+
+        private ContactsDatabase()
+        {
+            contactsEntities = new ContactsEntities1();
+            connection = contactsEntities.Database.Connection;
+
+            connection.Open();
+        }
+
+        public List<contacts> GetAllContacts()
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM contacts";
+
+            List<contacts> contactsList = new List<contacts>();
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    contacts contact = new contacts();
+                    contact.name = reader.GetValue(0).ToString();
+                    contact.birthdate = DateTime.Parse(reader.GetValue(1).ToString());
+                    contact.address = reader.GetValue(2).ToString();
+                    contactsList.Add(contact);
+                }
+            }
+            return contactsList;
+        }
+
+        public int InsertNewContact(contacts contact)
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = "INSERT INTO contacts (name, birthdate, address) " +
+                                    "VALUES ('" + contact.name + "', "
+                                    + DateToString(contact.birthdate) + ", "
+                                    + "'" + contact.address + "')";
+
+            try
+            {
+                int aff = command.ExecuteNonQuery();
+                return aff;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        private string DateToString(DateTime date)
+        {
+            return "'" + date.Year + "-" + date.Month + "-" + date.Day + "'";
+        }
+
+        public int DeleteContact(contacts contact)
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = "DELETE FROM contacts WHERE name = '"
+                                    + contact.name + "' AND address = '" + contact.address + "'";
+
+            // return value of ExecuteNonQuery (i) is the number of rows affected by the command
+            return command.ExecuteNonQuery();
+        }
+
+        public int UpdateContact(contacts contactToUpdate, contacts newContact)
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = "UPDATE contacts SET name = '" + newContact.name +
+                                    "', birthdate = " + DateToString(newContact.birthdate) +
+                                    ", address = '" + newContact.address + 
+                                    "' WHERE name = '" + contactToUpdate.name +
+                                    "' AND address = '" + contactToUpdate.address + "'";
+
+            // return value of ExecuteNonQuery (i) is the number of rows affected by the command
+            return command.ExecuteNonQuery();
+        }
+
+        public contacts GetPhoneNumbersForContact(contacts contact)
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM phonenumbers" + 
+                                    " WHERE name = '" + contact.name + "'";
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    phonenumbers phonenumber = new phonenumbers();
+                    phonenumber.number = reader.GetValue(0).ToString();
+                    phonenumber.name = reader.GetValue(1).ToString();
+                    phonenumber.contacts = contact;
+                    contact.phonenumbers.Add(phonenumber);
+                }
+            }
+            return contact;
+        }
+    }
+}
--- AssignmentInfinum/Controllers/ContactsController.cs
+++ AssignmentInfinum/Controllers/ContactsController.cs
@@ -1,4 +1,5 @@
-﻿using PagedList;
+﻿using Devart.Data.PostgreSql;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,78 +8,49 @@ using System.Web.Mvc;
 
 namespace AssignmentInfinum.Controllers
 {
+    [RoutePrefix("Contacts")]
     public class ContactsController : Controller
     {
-        List<contacts> contacts2;
         // GET: Contacts
         public ActionResult Index(int? page)
         {
-            ContactsEntities1 contactsEntities = new ContactsEntities1();
-
-
+            contacts c = ContactsDatabase.Instance.GetPhoneNumbersForContact(
+                new contacts() { address = "asd", name = "asd" });
 
             int pageSize = 3;
             int pageNumber = (page ?? 1);
 
-            contacts2 = new List<contacts>();
-            contacts2.Add(new contacts() { name = "asd", birthdate = new DateTime(1900, 1, 1), address = "lda" });
-            contacts2.Add(new contacts() { name = "sad", birthdate = new DateTime(1900, 1, 1), address = "da" });
-            contacts2.Add(new contacts() { name = "as", birthdate = new DateTime(1900, 1, 1), address = "la" });
-            contacts2.Add(new contacts() { name = "sd", birthdate = new DateTime(1900, 1, 1), address = "lds" });
-            contacts2.Add(new contacts() { name = "a", birthdate = new DateTime(1900, 1, 1), address = "l" });
-            contacts2.Add(new contacts() { name = "d", birthdate = new DateTime(1900, 1, 1), address = "a" });
-            contacts2.Add(new contacts() { name = "s", birthdate = new DateTime(1900, 1, 1), address = "ld" });
-            PagedList<contacts> contacts1 = new PagedList<contacts>(contacts2, pageNumber, pageSize);
-
-            return View(contacts2.ToPagedList(pageNumber, pageSize));
+            return View(ContactsDatabase.Instance.GetAllContacts().ToPagedList(pageNumber, pageSize));
         }
 
         [HttpPost]
         public JsonResult InsertContact(contacts contact)
         {
-            using (ContactsEntities1 contactsModel = new ContactsEntities1())
-            {
-                contactsModel.contacts.Add(contact);
-                contactsModel.SaveChanges();
-            }
-
+            ContactsDatabase.Instance.InsertNewContact(contact);
             return Json(contact);
         }
 
         [HttpPost]
-        public ActionResult UpdateContact(contacts contact)
+        public ActionResult UpdateContact(contacts contactOld, contacts contactNew)
         {
-            using (ContactsEntities1 contactsModel = new ContactsEntities1())
-            {
-                contactsModel.contacts.Add(contact);
-                contactsModel.SaveChanges();
-            }
+            ContactsDatabase.Instance.UpdateContact(contactOld, contactNew);
 
             return new EmptyResult();
         }
 
         [HttpPost]
-        public ActionResult DeleteContact(contacts contacts)
+        public ActionResult DeleteContact(contacts contact)
         {
-            using (ContactsEntities1 contactsModel = new ContactsEntities1())
-            {
-                //contacts deleteContact = (from c in contactsModel.contacts
-                //                                where c.name == name && c.address == address
-                //                                select c).FirstOrDefault();
-
-                //contactsModel.contacts.Remove(deleteContact);
-                contactsModel.SaveChanges();
-            }
+            ContactsDatabase.Instance.DeleteContact(contact);
 
             return new EmptyResult();
         }
 
-        [Route("Contacts/Test")]
+        [Route("Test")]
         [HttpPost]
         public ActionResult Test(contacts contacts)
         {
-            Console.WriteLine("gkgkkggk");
-            return new EmptyResult();
+            return new System.Web.Mvc.JsonResult() { Data = "tralala" };
         }
     }
 }
